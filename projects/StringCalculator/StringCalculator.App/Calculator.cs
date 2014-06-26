e6bb3d074a0d64cd49db15945515909cbcda4ea1
Caleb Tucker <calebt@gmail.com>
2014-06-26 14:21:38 -0500

    added string calc project

---
+++ projects/StringCalculator/StringCalculator.App/Calculator.cs
@@ -0,0 +1,15 @@
+﻿namespace StringCalculator.App
+{
+    public interface ICalculator
+    {
+        int add(string input);
+    }
+
+    public class Calculator : ICalculator
+    {
+        public int add(string input)
+        {
+            return -1;
+        }
+    }
+}
---
+++ projects/StringCalculator/StringCalculator.Specs/CalculatorSpecs.cs
@@ -0,0 +1,37 @@
+﻿using Machine.Specifications;
+using developwithpassion.specifications.rhinomocks;
+using developwithpassion.specifications.extensions;
+using StringCalculator.App;
+
+namespace StringCalculator.Specs
+{
+    [Subject(typeof (Calculator))]
+    public class CalculatorSpecs
+    {
+        public abstract class concern : Observes<ICalculator, Calculator>
+        {
+        }
+
+        public class when_input_is_one_number : concern
+        {
+            Because b = () =>
+                result = sut.add("1");
+
+            It should_return_that_number = () =>
+                result.ShouldEqual(1);
+
+            static int result;
+        }
+
+        public class when_input_is_two_numbers : concern
+        {
+            Because b = () =>
+                result = sut.add("1,2");
+
+            It should_add_the_numbers = () =>
+                result.ShouldEqual(3);
+
+            static int result;
+        }
+    }
+}
