9affb4d2fe57a107c4a3532523bb71f61f40b6e6
Michael Elkins <michael.dennis.elkins@gmail.com>
2014-07-02 12:02:31 -0500

    cleaned up calculator_spec

--- projects/string_calculator/spec/lib/core/calculator_spec.rb
+++ projects/string_calculator/spec/lib/core/calculator_spec.rb
@@ -2,17 +2,14 @@ require 'spec_helper'
 
 module Guild
   describe Calculator do
-
-    before(:each) do
-      @parser = double('parser')
-      @sut    = Calculator.new(@parser)
-    end
-
     describe '#add' do
       context 'when_passed_input' do
         it 'should_sum_up_the_input' do
-          @parser.stub(:parse).and_return([1,2,3])
-          expect(@sut.add('1,2,3')).to eql(6) 
+          parser = double('parser')
+          sut    = Calculator.new(parser)
+          parser.stub(:parse).and_return([1,2,3])
+
+          expect(sut.add('1,2,3')).to eql(6) 
         end
       end
     end
